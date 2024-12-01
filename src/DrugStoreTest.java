5a127a613119b4d141ae1a3a218a9796d3f0b6e6
awakenw0w <awakenwow42@gmail.com>
2024-12-01 15:16:09 +0300

    Create DrugStoreTest.java

---
+++ src/DrugStoreTest.java
@@ -0,0 +1,47 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// класс для тестирования функциональности класса drugstore
+public class DrugStoreTest {
+    private DrugStore drugStore; // объект аптеке для тестирования
+
+    // метод который выполняется перед каждым тестом
+    @BeforeEach
+    public void setUp() {
+        // инициализируем объект drugstore и заполняем его данными
+        drugStore = new DrugStore("Быстров");
+        List<String> drugs = List.of("Парацетамол", "Аспирин", "Анальгин");
+        List<Double> prices = List.of(100.0, 50.0, 30.0);
+        drugStore.Initialize(drugs, prices); // инициализация данных для аптеки
+    }
+
+    // тест метода store_info
+    @Test
+    public void testStoreInfo() {
+        // ожидаемое значение для информации об аптеке
+        String expectedInfo = "~~~Аптека Быстров~~~\nЛекарства:\n"
+                + " - Парацетамол: 100.0 руб.\n"
+                + " - Аспирин: 50.0 руб.\n"
+                + " - Анальгин: 30.0 руб.\n";
+        // проверка правильности возвращаемой информации
+        assertEquals(expectedInfo, drugStore.store_info());
+    }
+
+    // Тестируем метод expensive_drug()
+    @Test
+    public void testExpensiveDrug() {
+        double expectedMaxPrice = 100.0; // Ожидаемое значение для самой дорогой цены
+        // Проверка правильности возвращаемой цены
+        assertEquals(expectedMaxPrice, drugStore.expensive_drug());
+    }
+
+    // Тестируем метод total_cost()
+    @Test
+    public void testTotalCost() {
+        double expectedTotalCost = 100.0 + 50.0 + 30.0; // Ожидаемая стоимость всех лекарств
+        // Проверка правильности общей стоимости
+        assertEquals(expectedTotalCost, drugStore.total_cost());
+    }
+    }
