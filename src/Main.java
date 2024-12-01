046169cbf978e4b2268f7abcdf87f8ab23e7f901
awakenw0w <awakenwow42@gmail.com>
2024-12-01 11:53:21 +0300

    Update Main.java

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class DrugStore {
+    String name_store;
+    String name_drug;
+    String drug_price;
+
+    public DrugStore(String name_store, String name_drug, String drug_price) {
+        this.name_store = name_store;
+        this.name_drug = name_drug;
+        this.drug_price = drug_price;
+    }
+
+    public void store_info() {
+        System.out.println("Аптека: " + name_store + "\nЛекарства:\n");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        List
+    }
+}
