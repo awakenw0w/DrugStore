34894b41e285740b69bdf44778c8640f744b6d74
awakenw0w <awakenwow42@gmail.com>
2024-12-01 14:51:05 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Drug> drugs = new ArrayList<>();
-        drugs.add(new Drug("fdsfsda"));
 
+        // создание аптеки
+        DrugStore drugStore = new DrugStore("Быстров");
+        List<String> drugs = List.of("Парацетамол", "Аспирин", "Анальгин");
+        List<Double> prices = List.of(100.0, 50.0, 30.0);
+        drugStore.Initialize(drugs, prices);
+
+        // проверка работоспособности методов
+        System.out.println(drugStore.store_info());
+        System.out.println("Самое дорогое лекарство стоит: " + drugStore.expensive_drug() + " руб \n");
+        System.out.println("Стоимость всех лекарств: " + drugStore.total_cost() + " руб \n");
     }
+}
