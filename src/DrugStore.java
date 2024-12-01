f7260163e18c692e544d832709cc123756080b43
awakenw0w <awakenwow42@gmail.com>
2024-12-01 14:51:07 +0300

    Update DrugStore.java

--- src/DrugStore.java
+++ src/DrugStore.java
@@ -1,16 +1,50 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class DrugStore {
-    String name_store;
-    String name_drug;
-    String drug_price;
+    private String name_store; // название аптеки
+    private List<String> name_drug; // названия лекарств
+    private List<Double> drug_price; // стоимость лекарств
 
-    public DrugStore(String name_store, String name_drug, String drug_price) {
+    //конструктор
+    public DrugStore(String name_store) {
         this.name_store = name_store;
-        this.name_drug = name_drug;
-        this.drug_price = drug_price;
+        this.name_drug = new ArrayList<>();
+        this.drug_price = new ArrayList<>();
+    }
+    // инициализация всех полей
+    public void Initialize(List<String> names, List<Double> prices) {
+        this.name_drug = names;
+        this.drug_price = prices;
     }
 
-    public void store_info() {
-        System.out.println("Аптека: " + name_store);
+    // получаем полную инфомрацию об аптеке
+    public String store_info() {
+        String info = "~~~Аптека " + name_store + "~~~" + "\nЛекарства:\n";
+        for (int i = 0; i < name_drug.size(); i++) { // Итерируем по спискам лекарств и их цен
+            info += " - " + name_drug.get(i) + ": " + drug_price.get(i) + " руб.\n"; // Добавляем информацию о каждом аптечном препарате
+        }
+        return info; // Возвращаем полную информацию
     }
-}
+
+    // стоимость самого доргого лекарства
+    public double expensive_drug() {
+        double max_price = 0;
+        for (double price : drug_price) {
+            if (price > max_price) {
+                max_price = price;
+            }
+        }
+        return max_price;
+    }
+
+    // стоимость всех лекарств
+    public double total_cost() {
+        double totalCost = 0;
+        for (double price: drug_price) {
+            totalCost += price;
+        }
+        return totalCost;
+    }
+
 }
